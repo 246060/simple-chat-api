10842b11d3f47dc8fc4f4b9472c516ca37474bf8
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2022-03-08 14:37:00 +0900

    feat: add oauth2 resource server config

--- api-project/src/main/java/xyz/jocn/chat/common/config/SecurityConfig.java
+++ api-project/src/main/java/xyz/jocn/chat/common/config/SecurityConfig.java
@@ -1,8 +1,77 @@
 package xyz.jocn.chat.common.config;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import xyz.jocn.chat.common.util.TokenUtil;
+
+@Slf4j
+@RequiredArgsConstructor
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	// https://www.toptal.com/spring/spring-security-tutorial
+	// https://developer.okta.com/blog/2021/05/05/client-credentials-spring-security
 
+	private final TokenUtil tokenUtil;
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity
+			.cors()
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/token", "/token/refresh", "/users").permitAll()
+			.anyRequest().authenticated()
+			.and().oauth2ResourceServer().jwt().decoder(jwtDecoder())
+		;
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(tokenUtil.getJwtKey()).build();
+		// OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(audienceValidator());
+		// jwtDecoder.setJwtValidator(withAudience);
+		return jwtDecoder;
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		// Used by spring security if CORS is enabled.
+
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
+	}
 }

f5a2c40968e087230cf0d6deb138b292dc341ee3
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2022-03-18 15:17:07 +0900

    feat: modify config and add log in exception handlers

--- api-project/src/main/java/xyz/jocn/chat/common/exception/GlobalExceptionHandler.java
+++ api-project/src/main/java/xyz/jocn/chat/common/exception/GlobalExceptionHandler.java
@@ -34,8 +34,11 @@ import xyz.jocn.chat.common.dto.ErrorResponse;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(RoomException.class)
-	public ResponseEntity handleRoomException(RoomException ex, WebRequest request) {
-		log.error(ex.getMessage());
+	public ResponseEntity<?> handleRoomException(RoomException ex, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		HttpHeaders headers = new HttpHeaders();
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ErrorResponse error = new ErrorResponse(status.value(), ex.getMessage());
@@ -43,8 +46,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(AuthenticationException.class)
-	public ResponseEntity handleAuthenticationException(AuthenticationException ex, WebRequest request) {
-		log.error(ex.getMessage());
+	public ResponseEntity<?> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		HttpHeaders headers = new HttpHeaders();
 		HttpStatus status = HttpStatus.UNAUTHORIZED;
 		ErrorResponse error = new ErrorResponse(status.value(), ex.getMessage());
@@ -52,7 +58,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(ApiAccessDenyException.class)
-	public ResponseEntity handleApiAccessDenyException(ApiAccessDenyException ex, WebRequest request) {
+	public ResponseEntity<?> handleApiAccessDenyException(ApiAccessDenyException ex, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		HttpHeaders headers = new HttpHeaders();
 		HttpStatus status = HttpStatus.FORBIDDEN;
 		ErrorResponse error = new ErrorResponse(status.value(), ex.getMessage());
@@ -60,8 +70,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(ResourceAlreadyExistException.class)
-	public ResponseEntity handleResourceAlreadyExistException(ResourceAlreadyExistException ex, WebRequest request) {
-		log.error(ex.getMessage());
+	public ResponseEntity<?> handleResourceAlreadyExistException(ResourceAlreadyExistException ex, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		HttpHeaders headers = new HttpHeaders();
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ErrorResponse error = new ErrorResponse(status.value(), ex.getDescription(), ex.getMessage());
@@ -69,8 +82,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-		log.error(ex.getMessage());
+	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		HttpHeaders headers = new HttpHeaders();
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ErrorResponse error = new ErrorResponse(status.value(), ex.getDescription(), ex.getMessage());
@@ -78,8 +94,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity handleHighestException(Exception ex, WebRequest request) {
-		StringBuffer sb = new StringBuffer();
+	public ResponseEntity<?> handleHighestException(Exception ex, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
+		StringBuilder sb = new StringBuilder();
 		sb.append(String.format("%s : %s", ex.getClass().getSimpleName(), ex.getMessage()));
 		sb.append(System.lineSeparator());
 
@@ -99,66 +119,110 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleHttpMediaTypeNotSupported(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleHttpMediaTypeNotAcceptable(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers,
 		HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleMissingPathVariable(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleMissingServletRequestParameter(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleServletRequestBindingException(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleConversionNotSupported(ConversionNotSupportedException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleConversionNotSupported(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
 		HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleTypeMismatch(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleHttpMessageNotReadable(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotWritable(HttpMessageNotWritableException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleHttpMessageNotWritable(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		final String desc = "argument invalid";
 		ErrorResponse error = new ErrorResponse(status.value(), desc, ex.getBindingResult().getFieldErrors());
 		return this.handleExceptionInternal(ex, fail(error), headers, status, request);
@@ -167,24 +231,41 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMissingServletRequestPart(MissingServletRequestPartException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleMissingServletRequestPart(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
 		WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleBindException(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
 		HttpStatus status, WebRequest request) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleNoHandlerFoundException(ex, headers, status, request);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleAsyncRequestTimeoutException(AsyncRequestTimeoutException ex,
 		HttpHeaders headers, HttpStatus status, WebRequest webRequest) {
+		log.debug("exception handler : {}", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		log.error("exception message : {}", ex.getMessage());
+
 		return super.handleAsyncRequestTimeoutException(ex, headers, status, webRequest);
 	}
+
 }
